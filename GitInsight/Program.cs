612e6edb627786a4711804ccd197c8d88f0615c4
Emilia-Victoria <emiliav.helsted@gmail.com>
2022-10-25 14:32:35 +0200

    add frequencyMode

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibGit2Sharp;
+class Program {
+    static void Main(string[] args){
+        using (var repo = new Repository(args[0])){
+            var commits = repo.Commits.ToList();
+            Console.WriteLine(commits.Count());
+            foreach (var commit in commits){
+                Console.WriteLine(commit.Author.Name + " " + commit.Author.When);
+            }
+            FrequencyMode(commits);
+        }
+    }
+
+    static void FrequencyMode(List<Commit> list){
+        var q = list.GroupBy(
+            (item => item.Author.When.Date),
+            (key, elements) => new {
+                key = key, 
+                count = elements.Distinct().Count()
+            }
+        );
+        foreach (var commit in q){
+            Console.WriteLine(commit.key.ToShortDateString() + " " + commit.count);
+        }
+    }
+
+    static void AuthorMode(){
+
+    }
+}
