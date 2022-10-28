a7deb3d7ff0528197305240e417da6a032e6fe6b
emno <emno@itu.dk>
2022-10-28 12:16:17 +0200

    implement args switch on modes

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -3,12 +3,18 @@ class Program {
     static void Main(string[] args){
         using (var repo = new Repository(args[0])){
             var commits = repo.Commits.ToList();
-            Console.WriteLine(commits.Count());
-            foreach (var commit in commits){
-                Console.WriteLine(commit.Author.Name + " " + commit.Author.When);
+            
+            if(args.Count() <2){
+                return;
             }
-            FrequencyMode(commits);
-            AuthorMode(commits);
+
+            if(args[1] == "A"){
+                AuthorMode(commits);
+            }else if(args[1] == "F"){
+                FrequencyMode(commits);
+            }else{
+                return;
+            }
         }
     }
 
@@ -21,11 +27,11 @@ class Program {
             }
         );
         foreach (var commit in q){
-            Console.WriteLine(commit.key.ToShortDateString() + " " + commit.count);
+            Console.WriteLine("\t"+commit.count +" "+commit.key.ToShortDateString());
         }
     }
 
-    static void AuthorMode(List<Commit> list){
+    static void AuthorMode(IEnumerable<Commit> list){
         var q = list.GroupBy(
             (item => item.Author.Name),
             (key, elements) => new {
