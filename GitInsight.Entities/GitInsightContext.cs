faa166ef4d56f282785ea56f95b7a099b36fdd33
Amalie (amdh) <amdh@itu.dk>
2022-11-01 13:53:21 +0100

    Fill in GitInsightContext

--- GitInsight.Entities/GitInsightContext.cs
+++ GitInsight.Entities/GitInsightContext.cs
@@ -1,1 +1,23 @@
 namespace GitInsight.Entities;
+
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+using GitInsight.Core;
+
+public partial class GitInsightContext : DbContext {
+    public GitInsightContext(DbContextOptions<GitInsightContext> options)
+            : base(options)
+        {
+        }
+    public virtual DbSet<Author> Authors { get; set; } = null!;
+    public virtual DbSet<Repo> Repos { get; set; } = null!;
+    public virtual DbSet<Commit> Commits { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+}
