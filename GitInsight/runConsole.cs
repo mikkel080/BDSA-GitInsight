a93d3ce035771f0898dda456c752ef8f0b3d0502
Amalie (amdh) <amdh@itu.dk>
2022-11-11 14:06:39 +0100

    console main deleted

--- GitInsight/runConsole.cs
+++ GitInsight/runConsole.cs
@@ -1,9 +1,7 @@
 namespace GitInsight;
 
 public class runConsole{
-    public static void Main(string[] args){
-        var program = new Program();
-        var text = program.Run(args[0], args[1]);
-        Console.WriteLine(text);
+    public static void Main(String[] args){
+
     }
 }
