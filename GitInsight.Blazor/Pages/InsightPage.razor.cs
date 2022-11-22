22ac21f7ec5f77bc741ca5838fbf8c9331f28531
Rasmus Højmark Falden <rafa@itu.dk>
2022-11-22 13:14:32 +0100

    setup the skeleton for the Git insight razor page frontend.

---
+++ GitInsight.Blazor/Pages/InsightPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GitInsight.Blazor.Pages
+{
+    public partial class InsightPage
+    {
+        [Parameter]
+        public string org { get; set; } = "";
+
+        [Parameter]
+        public string proj { get; set; } = "";
+
+        private string mode { get; set; } = "";
+
+
+        private void fetchAnalysis()
+        {
+            if (org == "" || proj == "" || mode == "")
+                return;
+
+            //Fetch the data from the api.
+        }
+    }
+}
