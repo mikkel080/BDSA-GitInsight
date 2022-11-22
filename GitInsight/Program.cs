f321d7e134d7a18f2c3907a897e072e5b22936cb
Amalie (amdh) <amdh@itu.dk>
2022-11-22 12:16:38 +0100

    dotnet format

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -46,37 +46,37 @@ public sealed class Program
     public async Task<string> Run(string githubName, string repoName)
     {
         using (var repo = new Repository(getPathOrCloneRepo(githubName, repoName)))
-        { 
+        {
             int repoId;
             CheckForGitUpdates(repo);
             repoId = await CreateOrUpdateData(repo, repoName);
 
             //Cursed but easy way to get results
             var repoObject = _context.Repos.Where(r => r.Id == repoId).First();
-            
+
             return JsonConvert.SerializeObject(new CombinedResult(repoObject.FrequencyResult!, repoObject.AuthorResult!), Formatting.Indented);
         }
     }
 
     void CheckForGitUpdates(Repository repo)
     {
-            // Credential information to fetch
-            LibGit2Sharp.PullOptions options = new LibGit2Sharp.PullOptions();
-            options.FetchOptions = new FetchOptions();
-            options.FetchOptions.CredentialsProvider = new CredentialsHandler(
-            (url, usernameFromUrl, types) =>
-                new UsernamePasswordCredentials()
-                {
-                    Username = "USERNAME",
-                    Password = "PASSWORD"
-                });
+        // Credential information to fetch
+        LibGit2Sharp.PullOptions options = new LibGit2Sharp.PullOptions();
+        options.FetchOptions = new FetchOptions();
+        options.FetchOptions.CredentialsProvider = new CredentialsHandler(
+        (url, usernameFromUrl, types) =>
+            new UsernamePasswordCredentials()
+            {
+                Username = "USERNAME",
+                Password = "PASSWORD"
+            });
 
-            // User information to create a merge commit
-            var signature = new LibGit2Sharp.Signature(
-                new Identity("MERGE_USER_NAME", "MERGE_USER_EMAIL"), DateTimeOffset.Now);
+        // User information to create a merge commit
+        var signature = new LibGit2Sharp.Signature(
+            new Identity("MERGE_USER_NAME", "MERGE_USER_EMAIL"), DateTimeOffset.Now);
 
-            // Pull
-            Commands.Pull(repo, signature, options);  
+        // Pull
+        Commands.Pull(repo, signature, options);
     }
 
     async Task<int> CreateOrUpdateData(Repository repo, string RepoName)
@@ -86,16 +86,16 @@ public sealed class Program
         {
             await SaveDataAsync(repo, repoId);
         }
-        else 
+        else
         {
             var repoDTO = await _repositoryRepos.FindAsync(repoId);
             var latestDate = await _repositoryCommit.FindAsync(repoDTO.LatestCommit);
-            if (repo.Commits.First().Author.When.DateTime != latestDate.Date) 
+            if (repo.Commits.First().Author.When.DateTime != latestDate.Date)
             {
                 await UpdateDataAsync(repo, repoId, repoDTO);
             }
         }
-        return repoId; 
+        return repoId;
     }
 
     async Task<int> SaveDataAsync(Repository repo, int repoId)
@@ -112,17 +112,17 @@ public sealed class Program
 
     async Task<int> UpdateDataAsync(Repository repo, int repoId, RepoDTO repoDTO)
     {
-            var allCommits = await _repositoryCommit.ReadAsync();
-            var currentCommits = allCommits.Where(c => c.RepoId == repoId).Select(c => c.Date);
-            
-            foreach (var commit in repo.Commits.ToList())
+        var allCommits = await _repositoryCommit.ReadAsync();
+        var currentCommits = allCommits.Where(c => c.RepoId == repoId).Select(c => c.Date);
+
+        foreach (var commit in repo.Commits.ToList())
+        {
+            if (currentCommits.Contains(commit.Author.When.DateTime)) { continue; }
+            else
             {
-                if (currentCommits.Contains(commit.Author.When.DateTime)){continue;}
-                else 
-                {
-                    await _repositoryCommit.CreateAsync(new CommitCreateDTO(repoId, commit.Author.Name, commit.Author.When.DateTime));
-                }  
-            }    
+                await _repositoryCommit.CreateAsync(new CommitCreateDTO(repoId, commit.Author.Name, commit.Author.When.DateTime));
+            }
+        }
         await _repositoryRepos.UpdateAsync(new RepoUpdateDTO(repoDTO.Id, repoDTO.Name, repoDTO.LatestCommit, repoDTO.AllCommits));
         return repoId;
     }
