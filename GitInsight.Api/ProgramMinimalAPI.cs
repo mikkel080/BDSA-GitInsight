f596f8cabf1b69bb94988d302108db593ff06c7e
Amalie (amdh) <amdh@itu.dk>
2022-12-02 14:44:21 +0100

    Blazor calling api works

--- GitInsight.Api/ProgramMinimalAPI.cs
+++ GitInsight.Api/ProgramMinimalAPI.cs
@@ -34,8 +34,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
 
 var scopeRequiredByApi = app.Configuration["AzureAd:Scopes"] ?? "";
 
