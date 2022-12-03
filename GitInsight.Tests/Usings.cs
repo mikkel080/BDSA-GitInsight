cf74ade2f5b87059e05918381e68c57bae587824
Emil <emno@itu.dk>
2022-12-03 13:21:18 +0100

    remove .core from usings

--- GitInsight.Tests/Usings.cs
+++ GitInsight.Tests/Usings.cs
@@ -3,5 +3,4 @@ global using FluentAssertions;
 global using Microsoft.EntityFrameworkCore;
 global using GitInsight.Entities;
 global using Microsoft.Data.Sqlite;
-global using GitInsight.Core;
 global using Microsoft.Extensions.Configuration;
