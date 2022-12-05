62ee0ee6c046169889ffe31289a13066e4eca149
Amalie (amdh) <amdh@itu.dk>
2022-12-05 20:02:13 +0100

    Constructors in POCO

--- GitInsight.Blazor/Data/JSONPOCO.cs
+++ GitInsight.Blazor/Data/JSONPOCO.cs
@@ -6,17 +6,36 @@ namespace GitInsight.Blazor.Data
         public Frequencyresult FrequencyResult { get; set; }
         public Authorresult AuthorResult { get; set; }
         public Forkresult ForkResult { get; set; }
+
+        public Rootobject(Repositoryidentifier repositoryIdentifier, Frequencyresult frequencyResult, Authorresult authorResult, Forkresult forkResult)
+        {
+            RepositoryIdentifier = repositoryIdentifier;
+            FrequencyResult = frequencyResult;
+            AuthorResult = authorResult;
+            ForkResult = forkResult;
+        }
     }
 
     public class Repositoryidentifier
     {
         public string Organization { get; set; }
         public string Repository { get; set; }
+
+        public Repositoryidentifier(string organization, string repository)
+        {
+            Repository = repository;
+            Organization = organization;
+        }
     }
 
     public class Frequencyresult
     {
         public Datum[] Data { get; set; }
+
+        public Frequencyresult(Datum[] data)
+        {
+            Data = data;
+        }
     }
 
     public class Datum
@@ -28,17 +47,32 @@ namespace GitInsight.Blazor.Data
     public class Authorresult
     {
         public Datum1[] Data { get; set; }
+        public Authorresult(Datum1[] data)
+        {
+            Data = data;
+        }
     }
 
     public class Datum1
     {
         public string AuthorName { get; set; }
         public Frequencyresults FrequencyResults { get; set; }
+
+        public Datum1( string authorName, Frequencyresults frequencyresults)
+        {
+            AuthorName = authorName;
+            FrequencyResults = frequencyresults;
+        }
     }
 
     public class Frequencyresults
     {
         public Datum2[] Data { get; set; }
+
+        public Frequencyresults(Datum2[] data)
+        {
+            Data = data;
+        }
     }
 
     public class Datum2
@@ -50,6 +84,11 @@ namespace GitInsight.Blazor.Data
     public class Forkresult
     {
         public Repositoryidentifier[] RepositoryIdentifiers { get; set; }
+
+        public Forkresult(Repositoryidentifier[] repositoryidentifiers)
+        {
+            RepositoryIdentifiers = repositoryidentifiers;
+        }
     }
 
 }
